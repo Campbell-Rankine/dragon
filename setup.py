32213d2dff3f9344a9c4ca9cf96ad245a086e798
Campbell Rankine <campbellrankine@gmail.com>
2024-09-18 17:49:34 +1000

    once again

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ __author__ = "Campbell Rankine"
 
 setup(
     name="pytorch-dragon",
-    version="0.1.9",  # must match the github version
+    version="0.2.0",  # must match the github version
     author="Campbell Rankine",
     author_email="campbellrankine@gmail.com",
     description="A pytorch integrated Machine Learning / Deep learning utilities library",
-    download_url="https://github.com/Campbell-Rankine/dragon/archive/refs/tags/v0.1.9.tar.gz",
+    download_url="https://github.com/Campbell-Rankine/dragon/archive/refs/tags/v0.2.0.tar.gz",
     packages=find_packages(where="."),
     install_requires=[  # I get to this in a second
         "setuptools>=61.0",
